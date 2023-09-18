6977f1c1fd04be22de0ec2856469e9e7ba1e690e
shaeeb <shaeebali@gmail.com>
2023-09-18 10:21:06 +0100

    add topicsSlice feature

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const topicsSlice = createSlice({
+  initialState: {
+    topics: {},
+    reducers: {
+    addTopic: (state, payload) => {
+      const { topic } = action.payload.topic
+      state[topic].push(action.payload)
+    },
+    quizIds: [],
+   }
+  }
+})
