77671ea317446e6fb03d186cd17f1719c86ea135
shaeeb <shaeebali@gmail.com>
2023-09-18 11:54:01 +0100

    add quizzes slice

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -4,13 +4,14 @@ import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 // import selectors
+import { selectTopics } from "../features/topics/topicsSlice";
 
 export default function NewQuizForm() {
   const [name, setName] = useState("");
   const [cards, setCards] = useState([]);
   const [topicId, setTopicId] = useState("");
   const navigate = useNavigate();
-  const topics = {};  // Replace with topics 
+  const topics = useSelector(selectTopics);  // Replace with topics 
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
